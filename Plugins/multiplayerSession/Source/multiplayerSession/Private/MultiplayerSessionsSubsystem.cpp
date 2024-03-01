3882e093a415ff4ff545bcabae631a31d13a111a
LinYueMu <1466923976@qq.com>
2024-03-01 17:46:37 +0800

    1.创建对应的函数、委托、以及委托的句柄

--- Plugins/multiplayerSession/Source/multiplayerSession/Private/MultiplayerSessionsSubsystem.cpp
+++ Plugins/multiplayerSession/Source/multiplayerSession/Private/MultiplayerSessionsSubsystem.cpp
@@ -4,7 +4,12 @@
 #include "MultiplayerSessionsSubsystem.h"
 #include "OnlineSubsystem.h"
 
-UMultiplayerSessionsSubsystem::UMultiplayerSessionsSubsystem()
+UMultiplayerSessionsSubsystem::UMultiplayerSessionsSubsystem():
+CreateSessionCompleteDelegate(FOnCreateSessionCompleteDelegate::CreateUObject(this,&ThisClass::OnCreateSessionComplete)),
+FindSessionCompleteDelegate(FOnFindSessionsCompleteDelegate::CreateUObject(this,&ThisClass::OnFindSessionsComplete)),
+JoinSessionCompleteDelegate(FOnJoinSessionCompleteDelegate::CreateUObject(this,&ThisClass::OnJoinSessionComplete)),
+DestroySessionCompleteDelegate(FOnDestroySessionCompleteDelegate::CreateUObject(this,&ThisClass::OnDestroySessionComplete)),
+StartSessionCompleteDelegate(FOnStartSessionCompleteDelegate::CreateUObject(this,&ThisClass::OnStartSessionComplete))
 {
 	//Get() 是一个静态函数，它返回当前正在使用的在线子系统的实例，如果没有在线子系统可用，则返回空指针。
 	IOnlineSubsystem* Subsystem=IOnlineSubsystem::Get();
@@ -14,4 +19,44 @@ UMultiplayerSessionsSubsystem::UMultiplayerSessionsSubsystem()
 		// 在线会话接口 (IOnlineSession) 提供了管理多人游戏会话的功能，如创建、查找、加入和销毁游戏会话等
 		SessionInterface=Subsystem->GetSessionInterface();
 	}
+}
+
+void UMultiplayerSessionsSubsystem::CreateSession(int32 NumPublicConnections, FString MatchType)
+{
+}
+
+void UMultiplayerSessionsSubsystem::FindSessions(int32 MaxSearchResults)
+{
+}
+
+void UMultiplayerSessionsSubsystem::JoinSession(const FOnlineSessionSearchResult& SessionResult)
+{
+}
+
+void UMultiplayerSessionsSubsystem::DestroySession()
+{
+}
+
+void UMultiplayerSessionsSubsystem::StartSession()
+{
+}
+
+void UMultiplayerSessionsSubsystem::OnCreateSessionComplete(FName SessionName, bool bWasSuccessful)
+{
+}
+
+void UMultiplayerSessionsSubsystem::OnFindSessionsComplete(bool bWasSuccessful)
+{
+}
+
+void UMultiplayerSessionsSubsystem::OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result)
+{
+}
+
+void UMultiplayerSessionsSubsystem::OnDestroySessionComplete(FName SessionName, bool bWasSuccessful)
+{
+}
+
+void UMultiplayerSessionsSubsystem::OnStartSessionComplete(FName SessionName, bool bWasSuccessful)
+{
 }
--- Plugins/multiplayerSession/Source/multiplayerSession/Public/MultiplayerSessionsSubsystem.h
+++ Plugins/multiplayerSession/Source/multiplayerSession/Public/MultiplayerSessionsSubsystem.h
@@ -18,10 +18,37 @@ class MULTIPLAYERSESSION_API UMultiplayerSessionsSubsystem : public UGameInstanc
 
 public:
 	UMultiplayerSessionsSubsystem();
+
+	//当要通过菜单创建一个会话的时候，将会调用下面的函数
+	void CreateSession(int32 NumPublicConnections,FString MatchType);//创建一个会话，可参加人数+匹配条件
+	void FindSessions(int32 MaxSearchResults);//搜索已经存在的会话.存在的会话数目
+	void JoinSession(const FOnlineSessionSearchResult& SessionResult);//加入会话，返回的搜索到的会话结果
+	void DestroySession();//销毁会话
+	void StartSession();//启动会话？
+	
 protected:
+	//内部回调函数
+	void OnCreateSessionComplete(FName SessionName,bool bWasSuccessful);
+	void OnFindSessionsComplete(bool bWasSuccessful);
+	void OnJoinSessionComplete(FName SessionName,EOnJoinSessionCompleteResult::Type Result);
+	void OnDestroySessionComplete(FName	SessionName,bool bWasSuccessful);
+	void OnStartSessionComplete(FName SessionName,bool bWasSuccessful);
 
 private:
 	IOnlineSessionPtr SessionInterface;//IOnlineSessionPtr智能指针
 	
-	
+	//委托列表，将要绑定对应的回调函数给这些委托
+
+	FOnCreateSessionCompleteDelegate CreateSessionCompleteDelegate;
+	FOnFindSessionsCompleteDelegate FindSessionCompleteDelegate;
+	FOnJoinSessionCompleteDelegate JoinSessionCompleteDelegate;
+	FOnDestroySessionCompleteDelegate DestroySessionCompleteDelegate;
+	FOnStartSessionCompleteDelegate StartSessionCompleteDelegate;
+
+	//添加对应的委托句柄，作用相当于存储委托的变量
+	FDelegateHandle CreateSessionCompleteDelegateHandle;
+	FDelegateHandle FindSessionCompleteDelegateHandle;
+	FDelegateHandle JoinSessionCompleteDelegateHandle;
+	FDelegateHandle DestroySessionCompleteDelegateHandle;
+	FDelegateHandle StartSessionCompleteDelegateHandle;
 };
