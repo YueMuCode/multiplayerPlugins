63321bcfc5a09a563eb4e05b5f855a12a3f47292
LinYueMu <1466923976@qq.com>
2024-03-01 22:21:29 +0800

    1.在C++代码中测试了按钮的回调 2.使用指针在菜单类中与子系统实现了交互

--- Plugins/multiplayerSession/Source/multiplayerSession/Private/Menu_UserWidget.cpp
+++ Plugins/multiplayerSession/Source/multiplayerSession/Private/Menu_UserWidget.cpp
@@ -3,6 +3,9 @@
 
 #include "Menu_UserWidget.h"
 
+#include "MultiplayerSessionsSubsystem.h"
+#include "Components/Button.h"
+
 void UMenu_UserWidget::MenuSetup()
 {
 	AddToViewport();//将这个UI添加到视口
@@ -23,6 +26,63 @@ void UMenu_UserWidget::MenuSetup()
 			PlayerController->SetInputMode(InputModeData);
 			PlayerController->SetShowMouseCursor(true);//显示鼠标光针
 		}
+	}
+
+	//游戏实例（Game Instance）创建完成之后，会自动创建一个游戏实例子系统（Game Instance Subsystem）
+	//所有可以从GameInstance中访问到GameInstanceSubsystem
+	UGameInstance* GameInstance=GetGameInstance();
+	if(GameInstance)
+	{
+		MultiplayerSessionsSubsystem= GameInstance->GetSubsystem<UMultiplayerSessionsSubsystem>();
+	}
+}
+
+bool UMenu_UserWidget::Initialize()
+{
+	if(!Super::Initialize())
+	{
+		return false;
+	}
+	if(HostButton)
+	{
+		HostButton->OnClicked.AddDynamic(this,&ThisClass::HostButtonClicked);
+	}
+	if(JoinButton)
+	{
+		JoinButton->OnClicked.AddDynamic(this,&ThisClass::JoinButtonClicked);
+	}
+	return true;
+}
+
+void UMenu_UserWidget::HostButtonClicked()
+{
+	//屏幕调试信息
+	if(GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(
+			-1,
+			15.f,
+			FColor::Yellow,
+			FString((TEXT("创建按钮点击了，正在试图创建会话")))
+			);
+	}
+
+	if(MultiplayerSessionsSubsystem)
+	{
+		MultiplayerSessionsSubsystem->CreateSession(4,FString("FreeForAll"));//创建一个名为“FreeForAll”的容纳四个人的会话
+	}
+}
+
+void UMenu_UserWidget::JoinButtonClicked()
+{
+	if(GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(
+			-1,
+			15.f,
+			FColor::Yellow,
+			FString((TEXT("加入按钮点击了，正在试图加入会话")))
+			);
 	}
 }
                                                                                                 
--- Plugins/multiplayerSession/Source/multiplayerSession/Public/Menu_UserWidget.h
+++ Plugins/multiplayerSession/Source/multiplayerSession/Public/Menu_UserWidget.h
@@ -16,4 +16,27 @@ class MULTIPLAYERSESSION_API UMenu_UserWidget : public UUserWidget
 public:
 	UFUNCTION(BlueprintCallable)
 	void MenuSetup();
+
+protected:
+	virtual  bool Initialize() override;//为什么要使用Initialize
+	
+private:
+	UPROPERTY(meta=(BindWidget))//并指定该属性应该与一个 UI 控件进行绑定。这样，在使用该属性时，
+	//引擎会自动将其与相应的 UI 控件关联起来，方便你在代码中直接操作该控件。
+	//前提是：控件的名称 蓝图和C++中的必须一一致
+	class UButton* HostButton;
+
+	UPROPERTY(meta=(BindWidget))
+	 UButton* JoinButton;
+
+	//当按钮点击时需要完成的逻辑
+
+	UFUNCTION()
+	void HostButtonClicked();
+
+	UFUNCTION()
+	void JoinButtonClicked();
+
+	//菜单类中访问子系统的指针。
+	class UMultiplayerSessionsSubsystem* MultiplayerSessionsSubsystem;
 };
